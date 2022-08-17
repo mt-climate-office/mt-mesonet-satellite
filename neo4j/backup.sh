443e542f13f39a3fe3db6e661fef9231a4c67080
colinbrust <colin.brust@gmail.com>
2022-08-17 12:57:34 -0600

    use absolute neo4j path.

--- neo4j/backup.sh
+++ neo4j/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-neo4j stop
-neo4j-admin dump --database=neo4j --to=/dumps/neo4j/backup_$(date +%Y%m%d).dump
-neo4j start
+/var/lib/neo4j/bin/neo4j stop
+/var/lib/neo4j/bin/neo4j-admin dump --database=neo4j --to=/dumps/neo4j/backup_$(date +%Y%m%d%H).dump
+/var/lib/neo4j/bin/neo4j start
