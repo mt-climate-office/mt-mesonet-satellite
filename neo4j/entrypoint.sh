905a059936133e21c76ab645968b269d82f8b191
colinbrust <colin.brust@gmail.com>
2022-08-17 13:46:39 -0600

    Use neo4j entrypoitn at end.

--- neo4j/entrypoint.sh
+++ neo4j/entrypoint.sh
@@ -4,9 +4,10 @@ env >> ~/env.log
 echo 'PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' > /etc/crontab
 echo 'SHELL=/bin/bash' >> /etc/crontab
 
-echo '38 19 * * * root bash -c "source $HOME/env.log; source /var/lib/neo4j/backup.sh"' >> /etc/crontab
+echo '52 19 * * * root bash -c "source $HOME/env.log; source /var/lib/neo4j/backup.sh"' >> /etc/crontab
 echo '* * * * *  echo "Hello $(date)" >> /var/log/cron.log 2>&1' >> /etc/crontab
 
 service cron start
 neo4j start
-tail -f /dev/null
+
+exec /neo4j-entrypoint.sh "$@"
