ba59449c1585384ca10dc3a67680db27167cc1bd
colinbrust <colin.brust@gmail.com>
2022-08-17 15:21:52 -0600

    Edit cronjob.

--- neo4j/backup.sh
+++ neo4j/backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-/var/lib/neo4j/bin/cypher-shell -u $Neo4jUser -p $Neo4jPassword -d system "STOP DATABASE neo4j;"
+/var/lib/neo4j/bin/neo4j stop
 /var/lib/neo4j/bin/neo4j-admin dump --database=neo4j --to=/dumps/neo4j/backup_$(date +%Y%m%d%H%M).dump
-/var/lib/neo4j/bin/cypher-shell -u $Neo4jUser -p $Neo4jPassword -d system "START DATABASE neo4j;"
+/var/lib/neo4j/bin/neo4j start
